22a7b991624002e18619d941490d63e77a239aaf
figital <scott@figital.com>
2010-11-21 13:34:24 -0500

    favicon stubs, assorted randomness

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 <head>
 	<title>sand-dollar</title>
 	<script src="http://code.jquery.com/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/main.css" />
 </head>
 
 <body>
@@ -24,15 +25,18 @@
 	<i style="color:#999;">welcome to lorem ipsum dolor</i>
 	<p>
 
-	in leiu of content cached at the github wiki here's an introduction to  <a href="http://thirtythousand.org">thirtythousand.org</a>
+	in leiu of content cached at the github wiki here's an introduction to  <a href="http://thirtythousand.org">thirtythousand.org</a>:
 
 
-	<p>
+	<div class="box_shadow" style="border:1px solid #ccc;padding:20px;">
 	The framers of the Constitution and the Bill of Rights intended that the total population of Congressional districts never exceed 50 to 60 thousand. Currently, the average population size of the districts is nearly 700,000 and, consequently, the principle of proportionally equitable representation has been abandoned.
 	<p>
 	The historical trend relative to our federal Representation is illustrated in the charts below. The vertical bar chart illustrates that the total number of congressional districts was increased every ten years from 1790 to 1910 (with a single exception). These increases were a direct result of the growth in total population as was intended by the framers of the Constitution and the Bill of Rights.
  	<p>
 	Note that the last increase in representation occurred after the 1910 census when the total number of congressional districts was increased to 435. It has remained that size ever since (except for a four-year period when it was temporarily increased to 437 after the admission of Alaska and Hawaii).
+	</div>
+
+	<div id="footer"></div>
 
 </body>
 </html>
