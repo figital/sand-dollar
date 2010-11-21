a67da6faf5924d17c35337a5274b95170051e35a
figital <scott@figital.com>
2010-11-20 23:03:57 -0500

    basic login form prep

--- public_html/login.php
+++ public_html/login.php
@@ -1,1 +1,52 @@
-login.php
+<?
+    $pageTitle = "Login";
+
+
+	$message = "";
+	$error = false;
+
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+		if (!$error) {
+
+			try {
+				new dBug($_POST);
+
+			} catch (Exception $e) {
+
+				new dBug($e);
+
+			}
+
+		}
+
+	}
+?>
+<h2><?=$pageTitle?></h2>
+
+
+<?
+	if ($message) {
+		print_r("<div class='message bad'>".$message."</div>");
+	}
+?>
+
+<form method="post">
+
+	<div style="width:500px;border:1px dotted #39c;padding:20px;padding-bottom:35px;margin:15px;background-color:#f6f6f6;">
+
+	<div style="margin-bottom:5px;">
+		<div style="float:left;margin-right:10px;">
+		<div class="label">Username</div>
+		<input style="width:200px;font-size:20px;color:#666;padding-left:3px;" name="username" value="<?=$_POST['username']?>">
+		</div>
+		<div class="label">Password</div>
+		<input style="width:200px;font-size:20px;color:#666;padding-left:3px;" name="password" value="<?=$_POST['password']?>">
+
+		<input type="submit" name="submit" value="Submit" style="font-size:20px;">
+	</div>
+
+	</div>
+
+</form>
+
--- public_html/test/index.php
+++ public_html/test/index.php
@@ -1,7 +1,6 @@
 <?
 error_reporting(E_ALL);
 include("../../lib/php-github-api/phpGitHubApi.php");
-
 $github = new phpGitHubApi();
 $repo = $github->getRepoApi()->show('figital', 'sand-dollar');
 
@@ -15,14 +14,12 @@ $repo = $github->getRepoApi()->show('figital', 'sand-dollar');
 <h1><?=$repo["name"]?></h1>
 
 <ul>
-<li>
+<li><?=$repo["name"]?>
 <li><?=$repo["description"]?>
 <li><a href="<?=$repo["url"]?>"><?=$repo["url"]?></a>
 <li><a href="<?=$repo["homepage"]?>"><?=$repo["homepage"]?></a>
 </ul>
 
 <?
-echo '<b>dump:</b><xmp>';
-var_dump($repo);
-echo '</xmp>';
+new dBug($repo);
 ?>
