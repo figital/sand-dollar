86f2b55dbfda72509dfca3e5e4bf569e38a1c352
figital <scott@figital.com>
2010-12-01 23:44:44 -0500

    merged application.php

--- includes/application.php
+++ includes/application.php
@@ -3,11 +3,8 @@
 	error_reporting(E_ALL);
 
 	include("/var/www/sand-dollar/lib/dBug/dBug.php");
-<<<<<<< HEAD
-    include("/var/www/sand-dollar/lib/php-github-api/phpGitHubApi.php");
-=======
+	include("/var/www/sand-dollar/lib/php-github-api/phpGitHubApi.php");
 	
 	session_start();
 
->>>>>>> b482fb0ebe7775b2db263eb2c7b1a781265cc622
 ?>
